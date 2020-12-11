d25926137deb078f7e2f7575f0688d015c7c685d
tafar <tafar@1e0d2985-2c8d-4b09-9c31-68f1bf5835a3>
2020-12-11 10:28:28 +0000

    Added test file for queue.rkt and modified global.rkt
    git-svn-id: svn+ssh://atelier.inf.usi.ch/home/prendg/svn/pf1-project@16 1e0d2985-2c8d-4b09-9c31-68f1bf5835a3

--- modules/global.rkt
+++ modules/global.rkt
@@ -1,20 +1,20 @@
 
 ----------db------------
-database -> variable
-db_show -> accepts no arguments 
-(db_get-song a-song-id) -> accepts a-song-id 
-(db_add-song a-song [a-song-list (db_list-id "song")
-(db_get-song a-song-key)
-(check-album-empty a-song)
-(check-artist-empty a-song)
-(db_get-album a-album-key)
-(db_get-author a-author-key)
-(db_add-playlist playlist-name song-list)
-(db_get-playlist a-playlist-key)
-(db_add-single-to-playlist a-playlist-key a-song)
-(db_add-to-favorites a-song)
-(db_remove-from-favorites a-song)
-db_get-favorites-list -> accepts no arguments
+database -> variable ; creates a hash of the list that contains all the different types of song lists (albums, songs, authors, favorites, playlist)
+db_show -> accepts no arguments ; returns the datatbase
+(db_get-song a-song-id) -> accepts a-song-id ; takes the id of a soong and returns the song
+(db_add-song a-song [a-song-list (db_list-id "song") ; takes a song and adds it to the database
+(db_get-song a-song-key) ; takes the id of a soong and returns the song
+(check-album-empty a-song) ; checks if the song has an album in its data
+(check-artist-empty a-song) ; checks if the song has an author in its data
+(db_get-album a-album-key) ; returns the songs that are in an album
+(db_get-author a-author-key) ; returns the songs made by an author
+(db_add-playlist playlist-name song-list) ; sets a playlist to be played
+(db_get-playlist a-playlist-key) ; returns a playlist
+(db_add-single-to-playlist a-playlist-key a-song) ; adds a song to a playlist
+(db_add-to-favorites a-song) ; adds a song to favorites
+(db_remove-from-favorites a-song) ; removes a song from favorites
+db_get-favorites-list -> accepts no arguments ; returns the favorites list
 
 ---------player------------
 init-player -> variable
@@ -31,14 +31,14 @@ get-volume -> accepts no arguments
 current-time -> accepts no arguments
 
 -----------queue-------------
-QUEUE -> empty
-(enqueue_single element)
-(dequeue element)
-(removev2 element QUEUE)
-queue_first-> accepts no arguments
-queue_last -> accepts no arguments
-queue_show -> accepts no arguments
-queue_reset-> accepts no arguments
+QUEUE -> empty ; empty list, initial state of the queue(empty)
+(enqueue element) ; take a song and add it to the queue / take an album and add the songs to the queue
+(dequeue element) ; removes a song from the queue
+(removev2 element QUEUE) ; takes a list and an element in that list and removes it from the list
+queue_first-> accepts no arguments ; returns the first song in the queue
+queue_last -> accepts no arguments ; returns the last song in the queue
+queue_show -> accepts no arguments ; shows the entire queue
+queue_reset-> accepts no arguments ; sets the queue to its initial state(empty)
 
 -----------song----------------
 SONG [path tag audio-prop image-path]
